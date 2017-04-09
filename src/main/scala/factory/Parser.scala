20b101ed1addca2bb2d537abd2c2ff1c369e1d37
Angelo <angelooparah@gmail.com>
2017-04-09 14:26:10 +0100

    made return types coherent, improved error message and comments

--- src/main/scala/factory/Parser.scala
+++ src/main/scala/factory/Parser.scala
@@ -10,25 +10,26 @@ class Parser extends ProgramParser {
   val nameNumPattern = "([a-zA-Z]+) ([0-9]+)".r // space is represented by an actual space in the regex
   val namePattern = "([a-zA-Z]+)".r
 
-  def parse(file: String): Vector[Instruction] = doParsing(File(file).lines)
+  def parse(file: String): InstructionList = doParsing(File(file).lines)
 
   def parseString(string: String): InstructionList = doParsing(string.split('\n'))
 
 
-  /**
-   * Helper method to parse a string into an Instruction
- 	 * @param instructions
- 	 * 						collection of strings representing the intructions to be parsed
-   * @return a valid Instruction or throws an error
-   */
-  private def doParsing(instructions: TraversableOnce[String]): Vector[Instruction] = {
+/**
+  * Helper method to parse a collection of strings into an InstructionList
+  *
+  * @param instructions collection of strings representing the intructions to be parsed
+  * @return a collection of Instruction instances or throws an error
+  * @throws InvalidInstructionFormatException
+  */
+private def doParsing(instructions: TraversableOnce[String]): InstructionList = {
 
     for(toBeParsed <- instructions.toVector) yield {
 
       toBeParsed match {
         case namePattern(name) => new Instruction(name, Vector[Int]())
         case nameNumPattern(name, number) => new Instruction(name, Vector(number.toInt))
-        case _ => throw new InvalidInstructionFormatException("Invalid instruction format")
+        case _ => throw new InvalidInstructionFormatException(s"Invalid instruction format: ($toBeParsed)")
       }
     }
   }
