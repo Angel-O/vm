228f6611a9b76b7c262c872ea26078b503c33938
Angelo <angelooparah@gmail.com>
2017-04-09 14:22:45 +0100

    further improvements to parsing methods(reduced code duplication, using generic parameter)

--- src/main/scala/factory/Parser.scala
+++ src/main/scala/factory/Parser.scala
@@ -10,33 +10,26 @@ class Parser extends ProgramParser {
   val nameNumPattern = "([a-zA-Z]+) ([0-9]+)".r // space is represented by an actual space in the regex
   val namePattern = "([a-zA-Z]+)".r
 
-  def parse(file: String): Vector[Instruction] = {
+  def parse(file: String): Vector[Instruction] = doParsing(File(file).lines)
 
-    // create a file and parse each line
-    // TODO: is this efficient? will it generate a new file at each iteration?
-    for(line <- File(file).lines().toVector) yield doParsing(line)
-  }
-
-  def parseString(string: String): InstructionList = {
-
-    // turning the array to a vector since "yield"
-    // returns the same type of collection being iterated
-    for(line <- string.split('\n').toVector) yield doParsing(line)
-  }
+  def parseString(string: String): InstructionList = doParsing(string.split('\n'))
 
 
   /**
    * Helper method to parse a string into an Instruction
- 	 * @param string string representation of the Intruction to be parsed
+ 	 * @param instructions
+ 	 * 						collection of strings representing the intructions to be parsed
    * @return a valid Instruction or throws an error
    */
-  private def doParsing(string: String):Instruction = {
+  private def doParsing(instructions: TraversableOnce[String]): Vector[Instruction] = {
 
-    string match{
-        case nameNumPattern(name, number) => new Instruction(name, Vector(number.toInt))
-        // TODO: establish if this will be empty or contain a zero
+    for(toBeParsed <- instructions.toVector) yield {
+
+      toBeParsed match {
         case namePattern(name) => new Instruction(name, Vector[Int]())
+        case nameNumPattern(name, number) => new Instruction(name, Vector(number.toInt))
         case _ => throw new InvalidInstructionFormatException("Invalid instruction format")
+      }
     }
   }
 }
