9e92abbd0569ece091487a54f1b49cafea2fb755
Angelo <angelooparah@gmail.com>
2017-04-14 21:03:14 +0100

    added vm implementation stub

---
+++ src/main/scala/factory/VMachineParser.scala
@@ -0,0 +1,16 @@
+package factory
+
+import vm.VirtualMachineParser
+import bc.ByteCode
+
+class VMachineParser extends VirtualMachineParser {
+
+
+  def parse(file: String): Vector[ByteCode] = {
+    ???
+  }
+
+  def parseString(str: String): Vector[ByteCode] = {
+    ???
+  }
+}
