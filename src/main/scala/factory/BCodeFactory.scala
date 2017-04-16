d37b6c9927ea184b61551f68e8a3594947ac26a7
Angelo <angelooparah@gmail.com>
2017-04-16 08:21:50 +0100

    merged cases in bytecode factory

--- src/main/scala/factory/BCodeFactory.scala
+++ src/main/scala/factory/BCodeFactory.scala
@@ -13,14 +13,11 @@ class BCodeFactory extends Factory with ByteCodeValues {
 
   def make(byte: Byte, args: Int*): ByteCode = (byte, args size) match {
 
-    // valid binary bytecode
-    case (1, 1) => Iconst(args(0))
-
-    // binary bytecode with too few or too many args
-    case (1, _) => throw new IBE(s"'${names(0)}' instruction requires at least and at most one argument.")
+    // valid binary bytecode or invalid binary bytecode (too few or too many args)
+    case (1, size) => if (size == 1) Iconst(args(0)) else throw new IBE(s"'${names(0)}' bytecode requires at least and at most one argument.")
 
     // valid unary bytecode, (if it's unknown an IBE will be thrown)
-    case (b, 0) => byteToBytecode.getOrElse(b, throw new IBE(s"'$b' byte code is not valid."))
+    case (byte, 0) => byteToBytecode.getOrElse(byte, throw new IBE(s"'$byte' byte is not associated to a valid bytecode."))
 
     // invalid (e.g: negative or out of range) unary bytecode (or valid but with too many args)
     case _ => throw new IBE("invalid instruction.")
