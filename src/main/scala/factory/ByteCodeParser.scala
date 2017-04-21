5035e281ab129e406dc3dda2c4cf224530cc99c6
Angelo <angelooparah@gmail.com>
2017-04-21 13:03:47 +0100

    renamed scanvectors method to createBytecodes as that is what it really does

--- src/main/scala/factory/ByteCodeParser.scala
+++ src/main/scala/factory/ByteCodeParser.scala
@@ -9,7 +9,7 @@ class ByteCodeParser(factory: BCF) extends BCP {
   val Iconst = bytecode(names(0))
 
   // returning the second element of the pair
-  def parse(bytes: Vector[Byte]): Vector[ByteCode] = scanVectors(bytes)._2
+  def parse(bytes: Vector[Byte]): Vector[ByteCode] = createBytecodes(bytes)._2
 
 /**
   * This recursive method will do the heavy work and parse a Vector of Bytes into a Vector
@@ -20,17 +20,17 @@ class ByteCodeParser(factory: BCF) extends BCP {
   * 	containing the Bytecodes wanted: defaults to an empty vector
   * @return a pair of Vectors, where the second element of the pair is a Vector of `ByteCode` instances
   */
-def scanVectors(bytes: Vector[Byte], bytecodes: Vector[ByteCode] = Vector.empty): (Vector[Byte], Vector[ByteCode]) = bytes match {
+def createBytecodes(bytes: Vector[Byte], bytecodes: Vector[ByteCode] = Vector.empty): (Vector[Byte], Vector[ByteCode]) = bytes match {
 
     // if bytes contains at least two elements and the head is equal to Iconst we will
     // "consume" the head and the first element of the tail (ht) and keep scanning the tail
     // of the tail (tt)
-    case Iconst +: ht +: tt => scanVectors(tt, bytecodes :+ factory.make(Iconst, ht))
+    case Iconst +: ht +: tt => createBytecodes(tt, bytecodes :+ factory.make(Iconst, ht))
 
     // if bytes has an empty tail (aka single element) OR it has at least two elements but the head
     // is not equal to Iconst (this is the complemetary clause to the first match case) we will
     // "consume" the head of the bytes Vector and keep scanning its tail
-    case _+:_ | _+:_+:_ => scanVectors(bytes.tail, bytecodes :+ factory.make(bytes.head))
+    case _+:_ | _+:_+:_ => createBytecodes(bytes.tail, bytecodes :+ factory.make(bytes.head))
 
     // if bytes is empty we are done: return the pair of vectors
     case _ => (bytes, bytecodes)
